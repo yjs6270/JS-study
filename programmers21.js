2a29fe8d27f3ad2b32f1c6c570411d618386d777
jiseok <didwltjr00@naver.com>
2022-05-22 22:52:22 +0900

    js commit17

---
+++ programmers21.js
@@ -0,0 +1,11 @@
+function solution(absolutes, signs) {
+    var answer = 0;
+    for (var i = 0; i < signs.length; i++) {
+        if (signs[i] == true) {
+            answer += absolutes[i];
+        } else {
+            answer -= absolutes[i];
+        }
+    }
+    return answer;
+}
