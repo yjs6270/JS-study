c1925d20cfff80562751e8f007a0e56a33d0aa22
jiseok <didwltjr00@naver.com>
2022-06-07 22:31:45 +0900

    js commit31

---
+++ programmers35.js
@@ -0,0 +1,9 @@
+function solution(n, m) {
+    var answer = [];
+    const greatest = (a, b) => {
+        if (b === 0) return a
+        return greatest(b, a % b)
+    }
+    const least = (a,b) => (a*b) / greatest(a,b)
+    return [greatest(n,m), least(n,m)]
+}
